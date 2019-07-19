eeffada8b47ca7d5ef431b892ce62b74ac089626
3enit84 <45584131+3enit84@users.noreply.github.com>
2019-07-19 10:32:22 -0500

    Update Process_vcf_file_via_manifest.py

--- Process_vcf_file_via_manifest.py
+++ Process_vcf_file_via_manifest.py
@@ -1,24 +1,24 @@
-file_name = 'data/manifest-2019-07-16T15-56-46.245144/by-filename/NWD111050.freeze5.v1.vcf.gz'
+file_name = 'manifest-2019-07/by-filename/data.vcf.gz'
 filter_file = 'cardio.bed'
 
 from csv import reader
 import io
 filterfile = open(filter_file, 'r')
-flt_lst = []#filter list
+flt_lst = []#filter list of bed file
 for rec in reader(filterfile, delimiter='\t'):
     flt_lst += [[rec[0],int(rec[1]), int(rec[2])]]
 print('List is ready')
 filterfile.close()
 
-file = io.TextIOWrapper(gzip.open(file_name, 'r'))
-newfile = open('NWD111050_filtered.vcf','w')
+file = io.TextIOWrapper(gzip.open(file_name, 'r'))#open gz file in text format
+newfile = open('data_filtered.vcf','w')
 line = file.readline()
 newfile.write(line)
 while line != "":
     rec = str.split(line,'\t')
     for f in flt_lst:
         if f[0] == rec[0]:#the same chromosome
-            if f[1]<=int(rec[1])<=f[2]:
+            if f[1]<=int(rec[1])<=f[2]:#in the range of bed file entry
                 newfile.write(line)
     line = file.readline()
 file.close()
