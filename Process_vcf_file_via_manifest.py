b2e0bab458e4e6db9287586691e133f4e3212fc5
3enit84 <45584131+3enit84@users.noreply.github.com>
2019-07-19 10:54:23 -0500

    Update Process_vcf_file_via_manifest.py

--- Process_vcf_file_via_manifest.py
+++ Process_vcf_file_via_manifest.py
@@ -12,14 +12,11 @@ filterfile.close()
 
 file = io.TextIOWrapper(gzip.open(file_name, 'r'))#open gz file in text format
 newfile = open('data_filtered.vcf','w')
-line = file.readline()
-newfile.write(line)
-while line != "":
+for line in file:
     rec = str.split(line,'\t')
     for f in flt_lst:
         if f[0] == rec[0]:#the same chromosome
             if f[1]<=int(rec[1])<=f[2]:#in the range of bed file entry
                 newfile.write(line)
-    line = file.readline()
 file.close()
 newfile.close()  
