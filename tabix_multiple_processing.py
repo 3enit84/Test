a479b86c706fb4fe363ec0bcc906409af3bab817
3enit84 <45584131+3enit84@users.noreply.github.com>
2019-11-19 11:24:07 -0600

    Create tabix_multiple_processing.py

---
+++ tabix_multiple_processing.py
@@ -0,0 +1,17 @@
+import os, glob
+from multiprocessing import Pool
+
+FileNameLst = []
+for filename in glob.glob('*.vcf.gz'):
+        FileNameLst = FileNameLst + [filename]#make a list of file names
+print(len(FileNameLst), FileNameLst[0:4])
+
+def process_file(infile):#function to run each file through tabix
+        command = ('tabix -p vcf '+infile)
+        !{command}
+        return print(command)
+
+if __name__ == "__main__":
+    pool = Pool(8)#number of nodes to be used
+    results = pool.map(process_file, FileNameLst)
+    print('processed '+str(len(results))+' files')
