0fe0c4e75b3d839e13db53239a25aa388b7af31f
3enit84 <45584131+3enit84@users.noreply.github.com>
2019-11-22 13:55:15 -0600

    Update and rename Extract_7colimns_fromVCF.py to Extract_ALTcalls_fromVCF.py

--- Extract_ALTcalls_fromVCF.py
+++ Extract_ALTcalls_fromVCF.py
@@ -1,21 +1,24 @@
 import os, glob
 from multiprocessing import Pool
 from subprocess import call
+import gzip
 
 FileNameLst = []
-for filename in glob.glob('NWD*v1.vcf'):
+for filename in glob.glob('NWD*v1.vcf.gz'):
     FileNameLst = FileNameLst + [filename]#make a list of file names
 print(FileNameLst[0:4])
 def process_file(infile):#function to run each file through tabix
-    file = open(infile,'r')#read file in the list
-    outfile = open('shrt_'+infile,'w')
+    command = ('bgzip -d '+infile)#decompress bgzip file
+    call(command, shell = True)
+    file = open(infile[0:-3],'r')#read file in the list
+    outfile = open('shrt_'+infile[0:-3],'w')
     for line in file:
         if line[0] == "#":#skip header
             continue
         else:
             if str(1) in str.split(line,"\t")[9]:
                 outfile.write(line)
-                
+
 if __name__ == "__main__":
-    pool = Pool(8)#number of nodes to be used
+    pool = Pool(2)#number of nodes to be used
     results = pool.map(process_file, FileNameLst)
